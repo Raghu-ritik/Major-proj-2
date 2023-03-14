a536e6784e6a2ac9c55cf0c458eb04217cae76ca
ritu <ritukumarissm41@gmail.com>
2023-03-14 08:52:25 +0530

    know more page desing and implemendted

--- templates/user/knowmore.tpl
+++ templates/user/knowmore.tpl
@@ -1,1 +1,67 @@
-This is an About Us Page 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&family=Pacifico&display=swap" rel="stylesheet">
+
+<!-- Libraries Stylesheet -->
+<link href="{$g_base_url}/templates/assets/lib/animate/animate.min.css" rel="stylesheet">
+<link href="{$g_base_url}/templates/assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+<link href="{$g_base_url}/templates/assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+<!-- Customized Bootstrap Stylesheet -->
+<link href="{$g_base_url}/templates/assets/css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+<div class="container-xxl py-5">
+            <div class="container">
+                <div class="row g-5 align-items-center">
+                    <div class="col-lg-6">
+                        <div class="row g-3">
+                            <div class="col-6 text-start">
+                                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.1s" src="{$g_base_url}/templates/assets/myimages/about-1.jpg">
+                            </div>
+                            <div class="col-6 text-start">
+                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.3s" src="{$g_base_url}/templates/assets/myimages/about-2.jpg" style="margin-top: 25%;">
+                            </div>
+                            <div class="col-6 text-end">
+                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.5s" src="{$g_base_url}/templates/assets/myimages/about-3.jpg">
+                            </div>
+                            <div class="col-6 text-end">
+                                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.7s" src="{$g_base_url}/templates/assets/myimages/about-4.jpg">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <h4 class="section-title ff-secondary text-start text-primary fw-normal">About Us</h4>
+                        <h1 class="mb-4">Welcome to Cafeteen</h1>
+                        <p class="mb-3">
+                        The Food Order Application for Canteen is a web-based application designed to facilitate the ordering and management of food in canteens. </p>
+                        <p class="mb-3">The application allows customers to place their orders online and track the status of their orders. It also provides canteen staff with an intuitive interface for managing orders, updating menus, and tracking inventory. The application is designed to streamline the ordering process, reduce errors, and improve the overall customer experience. Additionally, it provides valuable insights into customer preferences and ordering patterns, which can help canteen operators optimize their menus and improve their offerings. Overall, the Food Order Application for Canteen is a powerful tool for improving the efficiency and effectiveness of food service in canteens. </p>
+                        <div class="row g-4 mb-4">
+                            <div class="col-sm-6">
+                                <div class="d-flex align-items-center border-start border-5 border-primary px-3">
+                                    <h1 class="flex-shrink-0 display-5 text-primary mb-0" data-toggle="counter-up">15</h1>
+                                    <div class="ps-4">
+                                        <p class="mb-0">Years of</p>
+                                        <h6 class="text-uppercase mb-0">Experience</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="d-flex align-items-center border-start border-5 border-primary px-3">
+                                    <h1 class="flex-shrink-0 display-5 text-primary mb-0" data-toggle="counter-up">7</h1>
+                                    <div class="ps-4">
+                                        <p class="mb-0">Popular</p>
+                                        <h6 class="text-uppercase mb-0">Master Chefs</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+            <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="{$g_base_url}/templates/assets/lib/wow/wow.min.js"></script>
+    <!-- Template Javascript -->
+    <script src="{$g_base_url}/templates/assets/js/knowmore.js"></script>
--- templates_c/ff2279974e73b8742d00e77a79a34549517cc2bd_0.file.knowmore.tpl.php
+++ templates_c/ff2279974e73b8742d00e77a79a34549517cc2bd_0.file.knowmore.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.0.0, created on 2023-03-12 18:50:19
+/* Smarty version 4.0.0, created on 2023-03-14 04:20:51
   from 'C:\xampp\htdocs\Major-proj-2\templates\user\knowmore.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.0.0',
-  'unifunc' => 'content_640e10db550993_43139048',
+  'unifunc' => 'content_640fe813542163_04802822',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'ff2279974e73b8742d00e77a79a34549517cc2bd' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Major-proj-2\\templates\\user\\knowmore.tpl',
-      1 => 1678643417,
+      1 => 1678764049,
       2 => 'file',
     ),
   ),
@@ -20,6 +20,88 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_640e10db550993_43139048 (Smarty_Internal_Template $_smarty_tpl) {
-?>This is an About Us Page <?php }
+function content_640fe813542163_04802822 (Smarty_Internal_Template $_smarty_tpl) {
+?><link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&family=Pacifico&display=swap" rel="stylesheet">
+
+<!-- Libraries Stylesheet -->
+<link href="<?php echo $_smarty_tpl->tpl_vars['g_base_url']->value;?>
+/templates/assets/lib/animate/animate.min.css" rel="stylesheet">
+<link href="<?php echo $_smarty_tpl->tpl_vars['g_base_url']->value;?>
+/templates/assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+<link href="<?php echo $_smarty_tpl->tpl_vars['g_base_url']->value;?>
+/templates/assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+<!-- Customized Bootstrap Stylesheet -->
+<link href="<?php echo $_smarty_tpl->tpl_vars['g_base_url']->value;?>
+/templates/assets/css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+<div class="container-xxl py-5">
+            <div class="container">
+                <div class="row g-5 align-items-center">
+                    <div class="col-lg-6">
+                        <div class="row g-3">
+                            <div class="col-6 text-start">
+                                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.1s" src="<?php echo $_smarty_tpl->tpl_vars['g_base_url']->value;?>
+/templates/assets/myimages/about-1.jpg">
+                            </div>
+                            <div class="col-6 text-start">
+                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.3s" src="<?php echo $_smarty_tpl->tpl_vars['g_base_url']->value;?>
+/templates/assets/myimages/about-2.jpg" style="margin-top: 25%;">
+                            </div>
+                            <div class="col-6 text-end">
+                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.5s" src="<?php echo $_smarty_tpl->tpl_vars['g_base_url']->value;?>
+/templates/assets/myimages/about-3.jpg">
+                            </div>
+                            <div class="col-6 text-end">
+                                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.7s" src="<?php echo $_smarty_tpl->tpl_vars['g_base_url']->value;?>
+/templates/assets/myimages/about-4.jpg">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <h4 class="section-title ff-secondary text-start text-primary fw-normal">About Us</h4>
+                        <h1 class="mb-4">Welcome to Cafeteen</h1>
+                        <p class="mb-3">
+                        The Food Order Application for Canteen is a web-based application designed to facilitate the ordering and management of food in canteens. </p>
+                        <p class="mb-3">The application allows customers to place their orders online and track the status of their orders. It also provides canteen staff with an intuitive interface for managing orders, updating menus, and tracking inventory. The application is designed to streamline the ordering process, reduce errors, and improve the overall customer experience. Additionally, it provides valuable insights into customer preferences and ordering patterns, which can help canteen operators optimize their menus and improve their offerings. Overall, the Food Order Application for Canteen is a powerful tool for improving the efficiency and effectiveness of food service in canteens. </p>
+                        <div class="row g-4 mb-4">
+                            <div class="col-sm-6">
+                                <div class="d-flex align-items-center border-start border-5 border-primary px-3">
+                                    <h1 class="flex-shrink-0 display-5 text-primary mb-0" data-toggle="counter-up">15</h1>
+                                    <div class="ps-4">
+                                        <p class="mb-0">Years of</p>
+                                        <h6 class="text-uppercase mb-0">Experience</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="d-flex align-items-center border-start border-5 border-primary px-3">
+                                    <h1 class="flex-shrink-0 display-5 text-primary mb-0" data-toggle="counter-up">7</h1>
+                                    <div class="ps-4">
+                                        <p class="mb-0">Popular</p>
+                                        <h6 class="text-uppercase mb-0">Master Chefs</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+            <!-- JavaScript Libraries -->
+    <?php echo '<script'; ?>
+ src="https://code.jquery.com/jquery-3.4.1.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['g_base_url']->value;?>
+/templates/assets/lib/wow/wow.min.js"><?php echo '</script'; ?>
+>
+    <!-- Template Javascript -->
+    <?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['g_base_url']->value;?>
+/templates/assets/js/knowmore.js"><?php echo '</script'; ?>
+><?php }
 }
